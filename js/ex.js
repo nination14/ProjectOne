64cd74c8f7abda13e808979b75b514201abf6832
Nina <ahirina.rivera@verizon.com>
2019-06-11 08:41:28 -0500

    Initial commit

---
+++ js/ex.js
@@ -0,0 +1,57 @@
+/*** Title */
+/****************************************
+Project 1- A Ramdom Quote Generator 
+Coder Name- Ahirina Rivera 06/08/2019
+*****************************************/
+
+
+
+/*** Array of Objects (Quotes) */
+var quotes = [ {
+     quote: "Disregard Females, Adquire Currency.", 
+     source: "The Notorius B.I.G.",
+     citation: "https://www.urbandictionary.com/define.php?term=Fuck%20Bitches%20Get%20Money",
+     year: "1995.", 
+},
+{
+    quote: " I'm Not A Great Programmer, I'm Just A Good Programmer With Great Habits.",
+    source: "Kent Beck.",
+    citation: "https://www.goodreads.com/author/quotes/25211.Kent_Beck",
+    year: "2012.", 
+},
+{
+    quote: "Bad Programmers Worry About The Code. Good Programmers Worry About The Data Structures And Their Relationships.",
+    source: "Linus Torvalds.",
+    citation: "https://quotefancy.com/programming-quotes",
+    year: "2006.",
+},
+{
+    quote: "Every Time You Fall Down, It Gives You An Opportunity To Question Yourself, Question Your Integrity. It's Not About The Actual Failure Itself — It's How You Respond To It.",
+    source: "Abby Wambach",
+    citation: "https://www.cosmopolitan.com/entertainment/news/g4873/most-inspiring-quotes-from-the-women-of-the-us-soccer-team/?slide=4",
+    year: "2009",
+},
+{
+    quote: "One Good Thing About Music, When It Hits You, You Feel No Pain.",
+    source: "Bob Marley",
+    citation: "https://www.cmuse.org/100-famous-and-inspirational-music-quotes/",
+    year: "1976",
+}
+];
+console.log(quotes) 
+
+/*** getRandomQuote Function */
+    function getRandomQuote ()  {
+      
+        var quotes = Math.floor(Math.random() * quotes.length);
+        for ( var i = 0; i <quotes.length; i++) {
+          var 
+        }
+        return ;
+    }
+
+/*** printQuote Function */
+    function printQuote (){
+
+
+    }
